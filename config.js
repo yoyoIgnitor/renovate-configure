50d11f60461a25a9c0ea0c3895c684a2e63fc05e
chandapukiran.kumar <chandapukiran.kumar@sentinelone.com>
2023-11-14 12:28:12 +0530

    renovate setup

---
+++ config.js
@@ -0,0 +1,16 @@
+const repos = JSON.parse(process.env.REPOSITORIES || '[]');
+
+module.exports = {
+  endpoint: "github.com",
+  repositories: repos.repositories,
+  onboardingConfig: {"$schema": "https://docs.renovatebot.com/renovate-schema.json"},
+  extends: ["local>gkiranch/renovate-configure"],
+  // username: 'gk-self-hosted-renovate[bot]',
+  gitAuthor:"GK self-hosted Renovate Bot <123456+gk-self-hosted-renovate[bot]@users.noreply.github.com>",  
+  "hostRules": [
+    {
+      "matchHost": "docker.io",
+      "hostType": "docker",       
+    }
+  ]
+}
