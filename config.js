35845d4f175d5fa7f21f634f82f1cc1ae866c608
chandapukiran.kumar <chandapukiran.kumar@sentinelone.com>
2023-11-22 19:14:37 +0530

    updated config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   platform: 'github',
   repositories: repos.repositories,
   onboardingConfig: {"$schema": "https://docs.renovatebot.com/renovate-schema.json"},
-  extends: ["local>gkiranch/renovate-configure"],
+  extends: ["local>yoyoIgnitor/renovate-configure"],
   // username: 'gk-self-hosted-renovate[bot]',
   gitAuthor:"GK self-hosted Renovate Bot <123456+gk-self-hosted-renovate[bot]@users.noreply.github.com>",  
   "hostRules": [
