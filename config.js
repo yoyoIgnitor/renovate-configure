5a4beb2494ba20f7632fb3c659babfed180c114a
chandapukiran.kumar <chandapukiran.kumar@sentinelone.com>
2023-11-28 15:32:24 +0530

    added maven

--- config.js
+++ config.js
@@ -15,6 +15,12 @@ module.exports = {
       "hostType": "docker",
       "username": process.env.RENOVATE_ART_SECRET_USERNAME,
       "password": process.env.RENOVATE_ART_SECRET_PASSWORD,            
+    },
+    {
+      "matchHost": "kiranrenovate.jfrog.io",
+      "hostType": "maven",
+      "username": process.env.RENOVATE_ART_SECRET_USERNAME,
+      "password": process.env.RENOVATE_ART_SECRET_PASSWORD,            
     }
   ]
-};
+}
